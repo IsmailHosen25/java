d0a7829dad260aa0530238fc76c19bc3c5af2a97
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2023-07-08 15:26:21 +0600

    prnt added

---
+++ prnt.java
@@ -0,0 +1,21 @@
+class prnt{
+  public static void main(String[] args) {
+    System.out.print("Hello world 1");
+    System.out.print("Hello world 2");
+    System.out.print("Hello world 3 \n");
+
+   //but if you want to print in secound line, then
+
+   System.out.println("Hello World 1");
+   System.out.println("Hello World 2");
+   System.out.println("Hello World 3");
+
+
+   //and another way
+
+   System.out.print("Hello world 1 \n");
+   System.out.print("Hello world 2 \n");
+   System.out.print("Hello world 3 \n");
+
+  };
+}
