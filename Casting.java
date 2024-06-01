2ac87540a6345b254c68e24c2abe19878cab0b62
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2024-06-01 11:00:32 +0600

    casting

---
+++ Casting.java
@@ -0,0 +1,12 @@
+public class Casting {
+    public static void main(String[] args) {
+        double price=100.00;
+        double finalprice =price+18;
+        System.out.println(finalprice);
+
+         System.out.println("");
+         int p=100;
+         int fp=p+(int)18.0;
+         System.out.println(fp);
+    }
+}
