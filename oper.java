d6f478b5ea8798f64c99a3e3758edcfd04ddea0f
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2024-06-01 11:23:33 +0600

    operator

---
+++ oper.java
@@ -0,0 +1,17 @@
+public class oper {
+    public static void main(String[] args) {
+        int a=10;
+        int b=8;
+        int sum =a+b;
+        System.out.println(sum);
+        System.out.println("");
+        int def =a-b;
+        System.out.println(def);
+        System.out.println("");
+        int dev =a/b;
+        System.out.println(dev);
+        System.out.println("");
+        double mod =a%b;
+        System.out.println(mod);
+    }
+}
